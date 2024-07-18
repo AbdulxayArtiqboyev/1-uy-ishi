3a5f553627c867fd392f187ad513e734288be2c5
AbdulxayArtiqboyev <ggithub164@gmail.com>
2024-07-18 17:18:41 +0500

    1 uy ishi

---
+++ app.js
@@ -0,0 +1,62 @@
+// {
+// let a = prompt('html nma');
+// console.log(`hyper text languages: ${a}`);
+// }
+
+// {
+// let a = prompt('css nma');
+//  console.log(`kaskading style sheets: ${a}`);
+// }
+// {
+// let a = prompt('js nma');
+//  console.log(`veb-brauzerdan tashqarida JavaScript kodini bajaradigan ochiq manbali, kross-platformali, JavaScript: ${a} `);
+// }
+
+// {
+// let a = prompt('responsive media nma');
+// console.log(`har xil qurilmalarga moslash websiteni: ${a}`);
+// }
+// {
+// let a = prompt('sup bilan sub taglarini farqi');
+// console.log(`sup tepaga , sub pasga chiqarib beradi kiritgan narsani: ${a}`);
+// }
+// {
+// let a = prompt('h1 tagi necha marotaba yoziladi');
+// console.log(`1 marotaba : ${a}`);
+// }
+// {
+// let a = prompt('html da nechta sarlavha taglari bor');
+// console.log(`6 ta:${a}`);
+// }
+// {
+// let a = prompt('rasm qoyish uchun qaysi tagdan foydalanamiz');
+// console.log(`img tagidan:${a}`);
+// }
+// {
+// let a = prompt('css da orqa fonga rang berish');
+// console.log(`backround color : ${a}`);
+// }
+// {
+//     let a = prompt('responsive media nma');
+//     console.log(`har xil qurilmalarga moslash websiteni: ${a}`);
+//     }
+//     {
+//     let a = prompt('sup bilan sub taglarini farqi');
+//     console.log(`sup tepaga , sub pasga chiqarib beradi kiritgan narsani: ${a}`);
+//     }
+//     {
+//     let a = prompt('h1 tagi necha marotaba yoziladi');
+//     console.log(`1 marotaba : ${a}`);
+//     }
+//     {
+//     let a = prompt('html da nechta sarlavha taglari bor');
+//     console.log(`6 ta:${a}`);
+//     }
+//     {
+//     let a = prompt('rasm qoyish uchun qaysi tagdan foydalanamiz');
+//     console.log(`img tagidan:${a}`);
+//     }
+//     {
+//     let a = prompt('css da orqa fonga rang berish');
+//     console.log(`backround color : ${a}`);
+//  }
